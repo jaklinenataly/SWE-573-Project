7d421103df91498df1cd8ec4280817462a1dd730
Vedat Akses <vedatakses@gmail.com>
2018-05-01 11:06:14 +0300

    Required changes for subscription impl

--- src/main/java/com/vakses/controller/SubscriptionController.java
+++ src/main/java/com/vakses/controller/SubscriptionController.java
@@ -1,30 +1,36 @@
 package com.vakses.controller;
 
+import com.vakses.model.resource.SubscriptionResource;
 import com.vakses.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.Set;
+
 /**
  * Created by veraxmedax on 23/03/2018.
  */
 @RestController
 @RequestMapping("/api/notification/subscriptions")
-public class NotificationSubscriptionController {
+public class SubscriptionController {
 
     private SubscriptionService subscriptionService;
 
     @Autowired
-    public NotificationSubscriptionController(SubscriptionService subscriptionService) {
+    public SubscriptionController(SubscriptionService subscriptionService) {
         this.subscriptionService = subscriptionService;
     }
 
     @PutMapping("/add")
+    //@PreAuthorize("hasAuthority('user.p')")
     public ResponseEntity<Boolean> addSubscription(@RequestParam(value = "userId") final String userId,
                                                    @RequestParam(value = "type") final String bloodGroupType,
                                                    @RequestParam(value = "location") final String location) {
@@ -35,4 +41,11 @@ public class NotificationSubscriptionController {
     public ResponseEntity<Boolean> removeSubscription(@PathVariable final long subscriptionId) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionService.removeSubscription(subscriptionId));
     }
+
+    @GetMapping()
+    //@PreAuthorize("hasRole('user.r')")
+    public ResponseEntity<Set<SubscriptionResource>> getUserSubscriptions(@RequestParam(value = "userId") final String userId, Principal principal) {
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionService.getSubscriptionsByUser(userId));
+    }
+
 }
---
+++ src/main/java/com/vakses/model/resource/SubscriptionResource.java
@@ -0,0 +1,13 @@
+package com.vakses.model.resource;
+
+import lombok.Data;
+
+/**
+ * Created by veraxmedax on 11/04/2018.
+ */
+@Data
+public class SubscriptionResource {
+    private long id;
+    private String bloodGroupType;
+    private String location;
+}
